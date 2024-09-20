34a2a887141c188a3fe6a7e37cf55cb9e4e9822b
Anup-Gandhi <anupgandhi81103@gmail.com>
2024-09-20 11:33:15 +0530

    Update index.tsx

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { Box, useMediaQuery } from "@mui/material";
-// import DashboardBox from "@/components/DashboardBox";
+import DashboardBox from "@/components/DashboardBox";
 import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
