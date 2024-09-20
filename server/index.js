f5ff10be67db0343466f6dacb1e5767f0141e19b
Anup-Gandhi <anupgandhi81103@gmail.com>
2024-09-20 21:37:28 +0530

    Backend set up

--- server/index.js
+++ server/index.js
@@ -4,4 +4,27 @@ import mongoose from 'mongoose';
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import morgan from "morgan"
+import morgan from "morgan";
+
+// configuration
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,         // Corrected from 'True' to 'true'
+        useUnifiedTopology: true,      // Corrected the spelling
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} did not connect`));
